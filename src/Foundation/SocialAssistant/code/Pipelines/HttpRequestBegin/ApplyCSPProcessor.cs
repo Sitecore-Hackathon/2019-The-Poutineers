f60cb5d63c4124a4f40f10db3a6d466b693e8438
aiswebmaster <dylan@allisolutions.com>
2019-03-02 15:36:30 -0800

    Attempt at CSP, running low on time

---
+++ src/Foundation/SocialAssistant/code/Pipelines/HttpRequestBegin/ApplyCSPProcessor.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Pipelines.HttpRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon.Foundation.SocialAssistant.Pipelines.HttpRequestBegin
+{
+    public class ApplyCSPProcessor : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            //args.HttpContext.Response.Headers.Add("Content-Security-Policy", "frame-src twitter.com");
+        }
+    }
+}
