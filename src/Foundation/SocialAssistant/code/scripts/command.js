c953e3e69b55b355a2229455a57e9829210f02cb
Dylan Young <dylany@gmail.com>
2019-03-01 20:57:36 -0800

    Creation of Command Button + Initial Serialization

---
+++ src/Foundation/SocialAssistant/code/scripts/command.js
@@ -0,0 +1,12 @@
+﻿define(["sitecore"], function (Sitecore) {
+    Sitecore.Commands.SocialAssist =
+        {
+            canExecute: function (context) {
+                // Determines whether command is disabled or enabled.
+                return true;
+            },
+            execute: function (context) {
+                alert("Hello world!");
+            }
+        };
+});
