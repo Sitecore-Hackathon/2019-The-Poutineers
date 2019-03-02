47c403cf08bda0b242a8a137761056a2ac9102fe
Dylan Young <dylany@connectivedx.com>
2019-03-02 02:40:06 -0800

    Refactored Experience Editor Button

---
+++ src/Foundation/SocialAssistant/code/sitecore/shell/client/Sitecore/ExperienceEditor/Commands/SocialAssistant.js
@@ -0,0 +1,18 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js", "jquery"], function (Sitecore, ExperienceEditor, $) {
+    Sitecore.Commands.SocialAssistant =
+        {
+            canExecute: function (context) {
+                // Determines whether command is disabled or enabled.
+                return true;
+            },
+            execute: function (context) {
+                var dialogWidth = '805px;';
+                var dialogHeight = '650px;';
+
+                var dialogFeatures = "dialogHeight: " + dialogHeight + "dialogWidth: " + dialogWidth;
+                ExperienceEditor.Dialogs.showModalDialog('http://www.twitter.com/', dialogFeatures, '', null, function (result) {
+
+                });
+            }
+        };
+});
