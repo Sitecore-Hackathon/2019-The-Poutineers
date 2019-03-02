bae005a0fff241ce7bc7c782fa419c1a79285d60
aiswebmaster <dylan@allisolutions.com>
2019-03-02 14:14:23 -0800

    Added an Endpoint for Twitter (Using TweetSharp) if FED side doesn't work

---
+++ src/Foundation/SocialAssistant/code/Controllers/TwitterController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TweetSharp;
+
+namespace Hackathon.Foundation.SocialAssistant.Controllers
+{
+    public class TwitterController : Controller
+    {
+        private readonly ITwitterService _twitterService;
+
+        public TwitterController(ITwitterService twitterService)
+        {
+            //_twitterService = twitterService;
+
+            //TODO: Handle DI
+            _twitterService = new TwitterService(new TwitterClientInfo() { ConsumerKey = "", ConsumerSecret = "" });
+        }
+
+        public JsonResult GetSearchResults(string query)
+        {
+            var results = _twitterService.Search(new SearchOptions() { Q = query });
+
+            return Json(results, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
